874e7731145542da28d56b7362b5c53d52bb1e42
Your Name <sagar.ugowdru@gmail.com>
2026-02-19 11:48:10 +0530

    new change in the index.tsx

--- index.tsx
+++ index.tsx
@@ -5,7 +5,11 @@ import App from './App';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
+<<<<<<< Updated upstream
   throw new Error("Could dgyg hg not find root element to mount to");
+=======
+  throw new Error("Could dgyg  jhafdm not find root element to mount to");
+>>>>>>> Stashed changes
 }
 
 const root = ReactDOM.createRoot(rootElement);
